e746733b95ca73a8197dbbe6f9ea47cdd1229ffe
Chaitanya Khandekar <chaitanyakhandekar95@gmail.com>
2025-06-30 20:08:23 +0530

    setup and basic utilities,models done and ready for development

---
+++ server/src/utils/generateAccessAndRefreshToken.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { ApiError } from "./apiError.js";
+import { ReturnDocument } from "mongodb";
+import { User } from "../models/user.model.js";
+
+export const generateAccessAndRefreshToken = async (userId)=>{
+    try {
+        
+        if(!userId || !mongoose.Types.ObjectId.isValid(userId)){
+            throw new ApiError(400,"Invalid UserID")
+        }
+
+        const user = await User.findById(userId)
+
+        if(!user){
+            throw new ApiError(400,"User does not exist")
+        }
+
+        const accessToken =  user.generateAccessToken()
+        const refreshToken =  user.generateRefreshToken()
+
+        user.refreshToken = refreshToken;
+
+        await user.save({validateBeforeSave:false})
+
+        return {accessToken,refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,error.message)
+    }
+}
