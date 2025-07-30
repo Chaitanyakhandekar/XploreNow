63793aee8f2da9b755d2af79b2451984781fd46d
Chaitanya <chaitanyakhandekar95@gmail.com>
2025-07-30 03:36:34 -0700

    sameSite=None

--- server/src/constants.js
+++ server/src/constants.js
@@ -6,7 +6,7 @@ const httpOnlyCookie = {
     secure: process.env.NODE_ENV === 'production', // Only secure in production
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     // Remove domain or make it dynamic
-    domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : undefined
+    // domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : undefined
 }
 export {
     dbName,
